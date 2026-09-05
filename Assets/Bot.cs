1a74dd4a6fc8a6a65b66b1d3943bf029acb7c919
carty <148784495+moononuu@users.noreply.github.com>
2026-09-05 15:26:50 +0100

    Evade

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -6,10 +6,12 @@ public class Bot : MonoBehaviour
 {
     NavMeshAgent agent;
     public GameObject target;
+    Drive ds;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
+        ds = target.GetComponent<Drive>();
     }
 
     void Seek(Vector3 location) 
@@ -32,19 +34,25 @@ public class Bot : MonoBehaviour
         float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
 
 
-        if ((toTarget > 90 && relativeHeading < 20) || target.GetComponent<Drive>().currentSpeed < 0.01f)
+        if ((toTarget > 90 && relativeHeading < 20) || ds.currentSpeed < 0.01f)
         {
             Seek(target.transform.position);
             return;
         }
 
-        float lookAhead = targetDir.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+        float lookAhead = targetDir.magnitude / (agent.speed + ds.currentSpeed);
         Seek(target.transform.position + target.transform.forward * lookAhead);
     }
 
+void Evade()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+        float lookAhead = targetDir.magnitude / (agent.speed + ds.currentSpeed);
+        Flee(target.transform.position + target.transform.forward * lookAhead);
+    }
 // Update is called once per frame
 void Update()
     {
-        Persue();
+        Evade();
     }
 }
