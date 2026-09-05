acd100cf7ed90f0758082ffb150539219dffbc53
carty <148784495+moononuu@users.noreply.github.com>
2026-09-05 15:05:24 +0100

    Flee code

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -18,9 +18,16 @@ public class Bot : MonoBehaviour
         agent.SetDestination(location);
     }   
 
+    void Flee(Vector3 location)
+    {
+        Vector3 fleeVector = location - this.transform.position;
+        agent.SetDestination(this.transform.position - fleeVector);  
+    }
+
+
 // Update is called once per frame
 void Update()
     {
-        Seek(target.transform.position);
+        Flee(target.transform.position);
 }
 }
